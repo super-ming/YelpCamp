0e21180ff20faa7cdd8f759542cc92ac9f061e30
Ming Ho <hkboi108@hotmail.com>
2019-03-17 23:19:46 -0700

    Updated author schema for comment model
    Update author to an object that stores the author id and and username so that the new comment form will automatically populate the author's username.

--- routes/comments.js
+++ routes/comments.js
@@ -8,7 +8,7 @@ const isLoggedIn = (req, res, next) => {
         return next();
     }
     res.redirect("/login");
-}
+};
 
 router.get("/new", isLoggedIn, (req, res)=> {
     Campground.findById(req.params.id, (err, campground) => {
@@ -30,6 +30,9 @@ router.post("/", isLoggedIn, (req, res) => {
                 if(err){
                     console.log(err);
                 } else {
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    comment.save();
                     campground.comments.push(comment);
                     campground.save();
                     res.redirect("/campgrounds/" + campground._id);
