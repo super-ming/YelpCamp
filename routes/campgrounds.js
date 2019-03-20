4022015b34fceb220ff8bc81d07ea468ffd566e8
Ming Ho <hkboi108@hotmail.com>
2019-03-20 15:31:26 -0700

    Added delete campground route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -74,6 +74,15 @@ router.put("/:id", (req, res) => {
     });
 })
 
+router.delete(":/id", (req, res) => {
+    Campground.findByIdAndRemove(req.params.id, (err) => {
+        if(err){
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    })
+})
 
 
 module.exports = router;
