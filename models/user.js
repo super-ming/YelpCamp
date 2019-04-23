2441d540eb77c030f60c783b059bd4fdbac105f7
Ming Ho <hkboi108@hotmail.com>
2019-04-22 17:26:48 -0700

    Update user model

--- models/user.js
+++ models/user.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
+  username: {type: String, unique: true, required: true},
+  password: String,
+  avatar: String,
+  firstName: String,
+  lastName: String,
+  email: {type: String, unique: true, required: true},
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  isAdmin: {type: Boolean, default: false}
 });
 
 UserSchema.plugin(passportLocalMongoose);
