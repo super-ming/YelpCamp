671d2bede86feb202928577ec2f9852aabc450fe
Ming Ho <hkboi108@hotmail.com>
2019-04-22 18:43:22 -0700

    Update user object with new fields

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const passport = require('passport');
 let User = require('../models/user');
 const middleware = require('../middleware');
+let Campground = require('../models/campground');
+let crypto = require('crypto');
 
 router.get("/", (req, res)=> {
     res.render("landing");
@@ -13,7 +15,18 @@ router.get("/register", (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-    let newUser = new User({ username: req.body.username })
+    let newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        avatar: req.body.avatar
+    });
+
+    if(req.body.adminCode === 'Hello world!') {
+      newUser.isAdmin = true;
+    }
+
     User.register(newUser, req.body.password, (err, user) => {
         if(err){
             req.flash("error", err.message);
