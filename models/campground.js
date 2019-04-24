cafc24bbc515f0b80b5b26e11a955604797afeb4
Ming Ho <hkboi108@hotmail.com>
2019-04-24 01:03:53 -0700

    Add input labels

--- models/campground.js
+++ models/campground.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const campgroundSchema = new mongoose.Schema({
     name: String,
-    price: String,
     image: String,
     description: String,
     cost: Number,
